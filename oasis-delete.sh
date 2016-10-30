3fde7d1fba9df3c22d03490f5b4e0fa9e1208e2a
EC2 Default User <ec2-user@ip-172-31-34-209.us-west-2.compute.internal>
2016-10-30 09:02:44 +0000

    init various kube test files

---
+++ oasis-delete.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kubectl delete deployments oasis-api-server
+kubectl delete svc oasis-api-server
+kubectl delete svc rabbitmq-service
+kubectl delete deployments oasis-rabbit
+kubectl delete deployments oasis-api-runner
+kubectl delete deployments oasis-mysql
+
