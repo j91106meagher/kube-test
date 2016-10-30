e0e4b0276087f9e02fea502e203ec7f87a43941d
EC2 Default User <ec2-user@ip-172-31-34-209.us-west-2.compute.internal>
2016-10-30 09:35:06 +0000

    add oasis worker setup

--- oasis-delete.sh
+++ oasis-delete.sh
@@ -6,4 +6,5 @@ kubectl delete svc rabbitmq-service
 kubectl delete deployments oasis-rabbit
 kubectl delete deployments oasis-api-runner
 kubectl delete deployments oasis-mysql
+kubectl delete deployments oasis-worker
 
